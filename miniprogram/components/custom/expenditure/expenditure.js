7dfed9a092caf79445fd0ed116d8312e5c0cd94e
471290430@qq.com <471290430@qq.com>
2019-03-18 22:17:25 +0800

    add expenditure selectedTime

--- miniprogram/components/custom/expenditure/expenditure.js
+++ miniprogram/components/custom/expenditure/expenditure.js
@@ -1,23 +1,35 @@
-// components/custom/expenditure/expenditure.js
+const util = require('../../../utils/util.js')
+const database = wx.cloud.database()
+
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
 
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    
   },
-
-  /**
-   * 组件的方法列表
-   */
+  lifetimes: {
+    attached() {
+      this.init()
+    }
+  },
   methods: {
-
+    dateChange: function (event) {
+      this.setData({
+        selectedDate: event.detail.value
+      })
+    },
+    init: function () {
+      this.initSelectedDate()
+    },
+    initSelectedDate: function () {
+      const date = new Date()
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      this.setData({
+        selectedDate: year + "-" + month + "-" + day,
+      })
+    },
   }
 })
